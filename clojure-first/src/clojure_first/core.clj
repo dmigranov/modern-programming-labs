32c5ae98e0f2c8e25daa3e24f9bd18be20618d46
Denis <dmigranov@DESKTOP-EVHPN3P.localdomain>
2020-09-16 16:49:23 +0700

    наклюнулась идея

--- clojure-first/src/clojure_first/core.clj
+++ clojure-first/src/clojure_first/core.clj
@@ -8,9 +8,26 @@
   (if (> arg 1) (* arg (fact (- arg 1))) 1))
 
 (defn words
-  [n]
+  [alphabet n]
+  (let [cardinality (count alphabet)])
+  (if (> n 1)
+    ()    ;then: взять words с n-1  и прибавить буковки слева справа
+    (alphabet)    ;else
+    )
+
   (list n (inc n))
   )
+
+(defn generate-word-list
+  [alphabet n word]
+  (if (< (.length word) n)
+    ()   ;then
+    (list)   ;else
+  )
+  )
+  
+
+
 
 (defn -main
   "I don't do a whole lot ... yet."
