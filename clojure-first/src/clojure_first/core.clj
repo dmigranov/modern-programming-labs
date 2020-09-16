8b2df62183927f3c6eb9b6f269f9ab4643c3f92b
Denis <dmigranov@DESKTOP-EVHPN3P.localdomain>
2020-09-16 15:39:28 +0700

    first

---
+++ clojure-first/src/clojure_first/core.clj
@@ -0,0 +1,26 @@
+(ns clojure-first.core
+  (:gen-class))
+
+
+
+(defn fact
+  [arg]
+  (if (> arg 1) (* arg (fact (- arg 1))) 1))
+
+(defn words
+  [n]
+  (list n (inc n))
+  )
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (def c 2)
+  (def result (+ c c))
+  
+  (println (fact result))
+  )
+
+
+
+
