63ef5a7d1b08b6cc41b1bd250c2ab38b9f370312
Denis <dmigranov@DESKTOP-EVHPN3P.localdomain>
2020-09-19 12:32:54 +0700

    более хорошая фия генерации словв

--- clojure-first/src/clojure_first/core.clj
+++ clojure-first/src/clojure_first/core.clj
@@ -19,7 +19,7 @@
   )
 
 
-;todo
+
 (defn generate-words
   [list n]
 
@@ -28,31 +28,39 @@
     list    ;else
     )
 
-  (list n (inc n)))
+  (list n (inc n))
+)
 
-
+;todo
+;;then: взять words с n-1  и прибавить буковки слева справа
 (defn generate-word-list
   [alphabet word n]
   (let [first-letter (get word 0)]) ;char
-  (if (> n 1)
-    (cons)    ;then: взять words с n-1  и прибавить буковки слева справа
-    list    ;else
-    )
 
-  (list n (inc n)))
+    (if (> (count alphabet) 0)
+      (cons (str (first alphabet) word)
+              (generate-word-list (rest alphabet) word n)) 
+      (list))    
+
+
+)
+
+(defn traverse-list [alphabet words n]
+  (
+   if (> (count words) 0)
+    (concat (generate-word-list alphabet (first words) n)
+          (traverse-list alphabet (rest words) n))
+    (list)
+  )
+)
 
 (defn generate-atomic-words
   [alphabet]
-  (if (> (count alphabet) 0)
+  (
+  if (> (count alphabet) 0)
     (cons (str (first alphabet)) (generate-atomic-words (rest alphabet)))
-    (list)))
-
-
-(defn traverse-list [alphabet words n]
-  (if (> (count words) 0)
-    (concat (generate-word-list alphabet (first words) 0)
-          (traverse-list alphabet (rest words) (dec n)))
-    (list))
+    (list)
+  )
 )
 
 ;!!!
