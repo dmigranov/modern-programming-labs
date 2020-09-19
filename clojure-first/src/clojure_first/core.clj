48d5ff197631b5e3c347580eae3434c3e62b5bcb
Denis <dmigranov@DESKTOP-EVHPN3P.localdomain>
2020-09-19 16:55:11 +0700

    удаление старого кода

--- clojure-first/src/clojure_first/core.clj
+++ clojure-first/src/clojure_first/core.clj
@@ -1,15 +1,6 @@
 (ns clojure-first.core
   (:gen-class))
 
-
-
-(defn fact
-  [arg]
-  (if (> arg 1) (* arg (fact (- arg 1))) 1))
-
-
-;todo
-;;then: взять words с n-1  и прибавить буковки слева справа
 (defn generate-word-list
   [alphabet word]
   (if (> (count alphabet) 0)
@@ -53,18 +44,10 @@
   (lenghthen-words alphabet (generate-atomic-words alphabet) n)
   )
 
-
-  
-
-
-
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (def c 2)
-  (def result (+ c c))
-  
-  (println (fact result))
+
+  (println (main-word-function '(\r \s) 2))
   )
 
 
