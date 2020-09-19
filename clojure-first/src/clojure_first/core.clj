2cd8699157e5e8d7e5e0fa43be5c008c157a240e
Denis <dmigranov@DESKTOP-EVHPN3P.localdomain>
2020-09-19 17:16:44 +0700

    некоторые переименования функций

--- clojure-first/src/clojure_first/core.clj
+++ clojure-first/src/clojure_first/core.clj
@@ -1,12 +1,11 @@
 (ns clojure-first.core
   (:gen-class))
 
-(defn generate-word-list
-  [alphabet word]
+(defn multiply-word [word alphabet]
   (if (> (count alphabet) 0)
     (concat
      (if (not= (get word 0) (first alphabet)) (list (str (first alphabet) word)) (list))
-     (generate-word-list (rest alphabet) word)
+     (multiply-word word (rest alphabet))
      )
     (list))
 )
@@ -14,22 +13,21 @@
 (defn traverse-list [alphabet words]
   (
    if (> (count words) 0)
-    (concat (generate-word-list alphabet (first words))
+    (concat (multiply-word (first words) alphabet)
           (traverse-list alphabet (rest words)))
     (list)
   )
 )
 
-(defn lenghthen-words [alphabet words n]
+(defn lengthen-words [alphabet words n]
   (cond 
-    (> n 2) (lenghthen-words alphabet (traverse-list alphabet words) (dec n))
+    (> n 2) (lengthen-words alphabet (traverse-list alphabet words) (dec n))
     (= n 2) (traverse-list alphabet words)
     :else words
     )
 )
 
-(defn generate-atomic-words
-  [alphabet]
+(defn generate-atomic-words [alphabet]
   (
   if (> (count alphabet) 0)
     (cons (str (first alphabet)) (generate-atomic-words (rest alphabet)))
@@ -37,19 +35,10 @@
   )
 )
 
-;!!!
-(defn main-word-function
-  [alphabet n]
-  ;(traverse-list alphabet (generate-atomic-words alphabet) n)
-  (lenghthen-words alphabet (generate-atomic-words alphabet) n)
-  )
+(defn generate-words [alphabet n] ;!!!entry!!!
+  (lengthen-words alphabet (generate-atomic-words alphabet) n)
+)
 
-(defn -main
-  [& args]
-
-  (println (main-word-function '(\r \s) 2))
-  )
-
-
-
-
+(defn -main [& args]
+  (println (generate-words '(\r \s) 2))
+)
