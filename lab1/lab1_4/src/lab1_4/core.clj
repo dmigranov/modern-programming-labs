069ad3d06db0ef9e7581a8164bfcdd1ba432950e
dmigranov <d.migranov@g.nsu.ru>
2020-09-26 19:09:08 +0700

    lab 1.4 done

--- lab1/lab1_4/src/lab1_4/core.clj
+++ lab1/lab1_4/src/lab1_4/core.clj
@@ -9,23 +9,25 @@
    (fn [alphabet-letter] (not= (get word 0) alphabet-letter)) ;predicate
    alphabet))
 
-(defn generate-lengthened-words [word alphabet]
-  (reduce (fn [coll letter] (concat coll (list (str letter word)))) (list) (filter-alphabet alphabet word)) 
+(defn generate-lengthened-words [word alphabet] ; 1 слово -> (|alphabet| - 1) удлиненных слов
+  (reduce (fn [coll letter] (concat coll (list (str letter word))))
+          (list)
+          (filter-alphabet alphabet word)) 
   )
 
 (defn reduce-func [words alphabet]
+  ; будет список списков, поэтому надо их все конкатенировать
   (reduce concat (map (fn [word]
-                  (generate-lengthened-words word alphabet)) words)))
+                        (generate-lengthened-words word alphabet)) words)))
 
 (defn my-repeat [n elem]
   (map (fn [num] elem) (range 0 n))) 
 
-;map reduce filter
 (defn generate-words [alphabet n] 
   (reduce reduce-func (generate-atomic-words alphabet) (my-repeat (dec n) alphabet))
-  ;reduce - подаем на вход список атомарных букв?
+  ;(reduce reduce-func (my-repeat n (generate-atomic-words alphabet))) ;можно так но надо переделать filter-alphabet
   )
 
 (defn -main
   [& args]
-  (println "Hello, World!"))
+  (println (generate-words '(\a \b \c \d) 5)))
