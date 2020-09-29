cea41224405ceb6126d318a24164a3ac58d476ea
dmigranov <d.migranov@g.nsu.ru>
2020-09-29 22:47:27 +0700

    написан прототип для функции интегрирования

--- lab2/lab2_1/src/lab2_1/core.clj
+++ lab2/lab2_1/src/lab2_1/core.clj
@@ -1,6 +1,13 @@
 (ns lab2-1.core
   (:gen-class))
 
+
+;0 to x: a = 0, b = x; h - шаг, n = x/h, тогда точки нумеруются [0, n] (включая n)
+(defn integrate-func [func x h]
+  
+  )
+
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
