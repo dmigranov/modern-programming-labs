8b82bb6db5a922c6f0c9f32b8b667840dcd73f57
dmigranov <d.migranov@g.nsu.ru>
2020-09-29 23:20:57 +0700

    исправил ашипку, теперь считает правильно, но условиям все равно не удовлетворяет потому integrate-func-simple

--- lab2/lab2_1/src/lab2_1/core.clj
+++ lab2/lab2_1/src/lab2_1/core.clj
@@ -5,12 +5,12 @@
 ;0 to x: a = 0, b = x; h - шаг, n = x/h, тогда точки нумеруются [0, n] (включая n)
 ;func(x)
 ;h = x/n
-(defn integrate-func [func x n]
+(defn integrate-func-simple [func x n]
   (let [h (double (/ x n))]
     (* h (reduce + (map (fn [grid-number]
                           (if (or (= grid-number 0) (= grid-number n))
-                            (/ (func (* n h)) 2.) ;then
-                            (func (* n h))))        ;else
+                            (/ (func (* grid-number h)) 2.) ;then
+                            (func (* grid-number h))))        ;else
                         (range 0 (inc n)))))
     ))
 
@@ -18,6 +18,5 @@
 
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
