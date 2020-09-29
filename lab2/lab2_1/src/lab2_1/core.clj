43b246f5662e5c7fa72b0308fde568a41f6e56ab
dmigranov <d.migranov@g.nsu.ru>
2020-09-29 23:01:38 +0700

    better idea but i have a even better idea

--- lab2/lab2_1/src/lab2_1/core.clj
+++ lab2/lab2_1/src/lab2_1/core.clj
@@ -5,8 +5,9 @@
 ;0 to x: a = 0, b = x; h - шаг, n = x/h, тогда точки нумеруются [0, n] (включая n)
 ;func(x)
 (defn integrate-func [func x h]
-  
-  (* h (+ (/ (+ (func 0) (func x)) 2.) (reduce + ())))
+  ;(+ (/ (+ (func 0) (func x)) 2.) (reduce + ()))
+
+  (* h (reduce + (map (fn [elem] elem) (range 0 (+ x h) h))))
   )
 
 
