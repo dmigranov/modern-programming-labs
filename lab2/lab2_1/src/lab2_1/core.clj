c38734e6e38ce44e65bc3f76372487069cbc8058
dmigranov <d.migranov@g.nsu.ru>
2020-10-03 17:10:57 +0700

    сделан пример в мейне для демонстрации правильности интегрирования экспоненты

--- lab2/lab2_1/src/lab2_1/core.clj
+++ lab2/lab2_1/src/lab2_1/core.clj
@@ -2,35 +2,24 @@
   (:gen-class))
 
 (defn square [x] (* x x))
-
 (defn exp [x] (Math/pow Math/E x))
-
 (defn sin [x] (Math/sin x))
-
 (def pi Math/PI)
 
 ;0 to x: a = 0, b = x; h - шаг, n = x/h (h = x/n), тогда точки нумеруются [0, n] (включая n)
-(defn integrate-no-memo [func x n]
+(defn integrate-no-memo-dumb [func x n]
   (let [h (double (/ x n))]
     (* h (reduce + (map (fn [grid-number]
                           (if (or (= grid-number 0) (= grid-number n))
                             (/ (func (* grid-number h)) 2.) ;then
                             (func (* grid-number h))))        ;else
-                        (range 0 (inc n)))))
-    ))
+                        (range 0 (inc n)))))))
 
 ;даёт площадь куска под функцией от a до b
 (defn trapezoid-rule [f a b]
+  ;(println (str "Calculating integral from "  a " to " b "...")) ;закоментить при замерах времени
   (* (- b a) (/ (+ (f a) (f b)) 2.)))
 
-(defn trapezoid-rule-debug [f a b]
-  (println (str "Calculating integral from "  a " to " b "..."))
-  (trapezoid-rule f a b))
-
-(defn calculate-integral-sum-no-memo [func k h]
-  (if (> k 0)
-    (+ (trapezoid-rule-debug func (* (dec k) h) (* k h)) (calculate-integral-sum-no-memo func (dec k) h))
-    0))
 
 (def calculate-integral-sum-memo (memoize (fn [func k h] 
                                       (if (> k 0)
@@ -45,13 +34,12 @@
     (calculate-integral-sum-memo func k h)
     ))
 
-
 (def fixed_h 0.1)
 (def epsilon (* fixed_h 1e-5))
-
 (defn close [x y]
   (< (Math/abs (- x y)) epsilon)
   )
+
 ;похоже, но если мы тыкаем за сетку, то досчитывается и прибавляется значение от ближайшего значения сетки 
 ;(но оно не мемоизируется, мемоизируется только значения интеграал для узлов сетки)
 (defn integrate-memo [func x h]
@@ -59,12 +47,10 @@
     (if (close x (* k h))
       (calculate-integral-sum-memo func k h) ;если пренебрежимо близко - то попали в сетку
       (let [l (Math/floor (/ x h))]    ;иначе округляем x/h вниз (l) и считаем интеграл от l*h до x и плюсуем 
-        (+ 
+        (+
          (calculate-integral-sum-memo func l h)
          (trapezoid-rule func (* l h) x) ;интеграл от l*h до x
-         ))
-      )
-    ))
+         )))))
 
 (defn integration-operator
   ([func step] (fn [x]
@@ -73,6 +59,25 @@
   )
 
 
+
+
+
+(defn calculate-integral-sum-no-memo [func k h]
+  (if (> k 0)
+    (+ (trapezoid-rule func (* (dec k) h) (* k h)) (calculate-integral-sum-no-memo func (dec k) h))
+    0))
+
+(defn integrate-no-memo [func x h]
+  )
+
+(defn integration-operator-no-memo
+  ([func step] (fn [x]
+                 (integrate-no-memo func x step)))
+  ([func] (integration-operator-no-memo func fixed_h)))
+
+
 (defn -main
   [& args]
-  (println "Hello, World!"))
+  (let [values (range 0.0 0.5 0.02)]
+    (println (map dec (map exp values)))
+    (println (map (integration-operator exp) values))))
