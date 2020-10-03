8107f70b7288e1199987eccd6c5b5d6db2482f5b
dmigranov <d.migranov@g.nsu.ru>
2020-10-03 17:20:10 +0700

    закончены варианты функций без мемоизации

--- lab2/lab2_1/src/lab2_1/core.clj
+++ lab2/lab2_1/src/lab2_1/core.clj
@@ -12,7 +12,6 @@
   ;(println (str "Calculating integral from "  a " to " b "...")) ;закоментить при замерах времени
   (* (- b a) (/ (+ (f a) (f b)) 2.)))
 
-
 (def calculate-integral-sum-memo (memoize (fn [func k h] 
                                       (if (> k 0)
                                         (+ (trapezoid-rule func (* (dec k) h) (* k h)) (calculate-integral-sum-memo func (dec k) h))
@@ -28,9 +27,7 @@
 
 (def fixed_h 0.1)
 (def epsilon (* fixed_h 1e-5))
-(defn close [x y]
-  (< (Math/abs (- x y)) epsilon)
-  )
+(defn close [x y] (< (Math/abs (- x y)) epsilon))
 
 ;похоже, но если мы тыкаем за сетку, то досчитывается и прибавляется значение от ближайшего значения сетки 
 ;(но оно не мемоизируется, мемоизируется только значения интеграал для узлов сетки)
@@ -39,16 +36,12 @@
     (if (close x (* k h))
       (calculate-integral-sum-memo func k h) ;если пренебрежимо близко - то попали в сетку
       (let [l (Math/floor (/ x h))]    ;иначе округляем x/h вниз (l) и считаем интеграл от l*h до x и плюсуем 
-        (+
-         (calculate-integral-sum-memo func l h)
-         (trapezoid-rule func (* l h) x) ;интеграл от l*h до x
-         )))))
+        (+ (calculate-integral-sum-memo func l h) (trapezoid-rule func (* l h) x))))))
 
-(defn integration-operator
+(defn integration-operator-memo
   ([func step] (fn [x]
                  (integrate-memo func x step)))
-  ([func] (integration-operator func fixed_h))
-  )
+  ([func] (integration-operator-memo func fixed_h)))
 
 
 
@@ -60,20 +53,23 @@
     0))
 
 (defn integrate-no-memo [func x h]
-  )
+  (let [k (Math/round (/ x h))]
+    (if (close x (* k h))
+      (calculate-integral-sum-no-memo func k h)
+      (let [l (Math/floor (/ x h))]
+        (+ (calculate-integral-sum-no-memo func l h) (trapezoid-rule func (* l h) x))))))
 
 (defn integration-operator-no-memo
   ([func step] (fn [x]
                  (integrate-no-memo func x step)))
   ([func] (integration-operator-no-memo func fixed_h)))
 
-
 (defn -main
   [& args]
   (let [values (range 0.0 0.5 0.02)]
     (println "e^x: expected vs what we got")
     (println (map dec (map exp values)))
-    (println (map (integration-operator exp) values))
+    (println (map (integration-operator-memo exp) values))
     (println "cos(x): expected vs what we got")
     (println (map sin values)) ;интеграл косинуса - синус
-    (println (map (integration-operator cos) values))))
+    (println (map (integration-operator-memo cos) values))))
