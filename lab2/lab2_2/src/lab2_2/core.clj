189e0de7fe797f7fa480378478933c5fac696c6d
dmigranov <d.migranov@g.nsu.ru>
2020-10-07 11:32:21 +0700

    наброски по поводу lazy-seq

--- lab2/lab2_2/src/lab2_2/core.clj
+++ lab2/lab2_2/src/lab2_2/core.clj
@@ -1,7 +1,35 @@
 (ns lab2-2.core
   (:gen-class))
 
+
+;площадь куска под функцией от a до b
+(defn trapezoid-rule [f a b]
+  ;(println (str "Calculating integral from "  a " to " b "..."))
+  (* (- b a) (/ (+ (f a) (f b)) 2.)))
+
+
+
+(defn integrate-lazy [func x h]
+
+  (let 
+   [
+    ;lazy-integral-sums (lazy-seq ())
+    lazy-integral-sums (iterate (fn [current-integral-sum] (+ current-integral-sum)) 0)
+    ]
+    
+    
+    
+    
+    ))
+
+(def fixed_h 0.1)
+(defn integration-operator
+  ([func step] (fn [x]
+                 (integrate-lazy func x step)))
+  ([func] (integration-operator func fixed_h)))
+
+
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
