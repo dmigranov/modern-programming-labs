81503449a2d159bddb0d9130c5406683b7bbdb6f
dmigranov <d.migranov@g.nsu.ru>
2020-10-10 18:17:06 +0700

    my-partition

--- lab3/lab3_1/src/lab3_1/core.clj
+++ lab3/lab3_1/src/lab3_1/core.clj
@@ -5,8 +5,20 @@
   
   )
 
+(defn partition-reduce-func [n]
+  (fn [coll elem])
+  )
+
+
+(defn divide-collection [n acc rest]
+  (if (> (count rest) 0)
+    (recur n (concat acc (list (take n rest))) (drop n rest))
+    acc))
+
 (defn my-partition [n coll] ;каждый кусок из n элементов
   ;take and drop
+  ;может, заредбюсить?
+  (divide-collection n (list) coll)
   )
 
 
