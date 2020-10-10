4bae0a9b20f16fb450a66b5716ec5ab20f69dba3
dmigranov <d.migranov@g.nsu.ru>
2020-10-10 18:27:45 +0700

    потер старье + думки

--- lab3/lab3_1/src/lab3_1/core.clj
+++ lab3/lab3_1/src/lab3_1/core.clj
@@ -5,22 +5,17 @@
   
   )
 
-(defn partition-reduce-func [n]
-  (fn [coll elem])
-  )
-
-
 (defn divide-collection [n acc rest]
   (if (> (count rest) 0)
     (recur n (concat acc (list (take n rest))) (drop n rest))
     acc))
 
 (defn my-partition [n coll] ;каждый кусок из n элементов
-  ;take and drop
-  ;может, заредбюсить?
   (divide-collection n (list) coll)
   )
 
+;deref = @
+;When applied to a future, will block if computation not complete. then return value
 
 (defn -main
   [& args]
