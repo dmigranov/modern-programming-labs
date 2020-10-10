e44f8a5e7ad1f7a0e16f7eb6ab53b181a7c07118
dmigranov <d.migranov@g.nsu.ru>
2020-10-10 17:19:46 +0700

    заготовки-прототипы функций

--- lab3/lab3_1/src/lab3_1/core.clj
+++ lab3/lab3_1/src/lab3_1/core.clj
@@ -1,7 +1,15 @@
 (ns lab3-1.core
   (:gen-class))
 
+(defn filter-future [pred coll]
+  
+  )
+
+(defn my-partition [n coll] ;каждый кусок из n элементов
+  ;take and drop
+  )
+
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
