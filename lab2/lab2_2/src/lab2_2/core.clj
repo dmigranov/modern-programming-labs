999611e2f9361f4cbdae82b153fe9ef2af7488d5
dmigranov <d.migranov@g.nsu.ru>
2020-10-10 17:06:18 +0700

    lab 2.2 done

--- lab2/lab2_2/src/lab2_2/core.clj
+++ lab2/lab2_2/src/lab2_2/core.clj
@@ -8,12 +8,9 @@
 
 
 
-
-
 (def fixed_h 0.1)
 (defn close [x y epsilon] (< (Math/abs (- x y)) epsilon))
 (def h-eps 1e-5) ;множитель на который умножается эпсилон при проверке близости к сетке
-
 
 
 (defn trapezoid-rule [f a b]
@@ -30,40 +27,19 @@
             [0., 0.])
    (map second)))
 
-;(declare lazy-sums)
 (defn integrate-lazy [func x h lazy-sums]
   (let [k (Math/round (/ x h))]
     (if (close x (* k h) (* h h-eps))
       (nth lazy-sums k) ;then - просто тупо берем из списка
       (let [l (Math/floor (/ x h))] (+ (nth lazy-sums l) (trapezoid-rule func (* l h) x))))))
 
-; два варианта
-; 1-ый (плохой, тк считает повторно):
-; lazy-list-of-integral-sums вызывается много раз
-; для каждого вызова возвращенной ФУНКЦИИ fn (lazy-list-of-integral-sums func step) вызывается ЗАНОВО (так она определена!)
-; то есть при применении к каждому элементу в map!
-(defn integration-operator-slow
-  ([func step] (fn [x] (integrate-lazy func x step (lazy-list-of-integral-sums func step))))
-  ([func] (integration-operator-slow func fixed_h)))
-
-; 2-ый - повторного пересчитывания нет, все считается оптимально:
-; (lazy-list-of-integral-sums func step) вызывается единожды для всех вызовов
-; почему так будет тут: при вызове варианта с двумя аргументами 
-; сначала вычислятся func, step и (lazy-list-of-integral-sums func step)
-; затем к ним применится integration-operator
-; возвращающая функцию, зависящую от уже вычисленной lazy-sums = (lazy-list-of-integral-sums func step)
 (defn integration-operator
   ([func step lazy-sums] (fn [x] (integrate-lazy func x step lazy-sums)))
   ([func step] (integration-operator func step (lazy-list-of-integral-sums func step)))
   ([func] (integration-operator func fixed_h)))
 
 (defn calc-integral-graphic-lazy [func integration-step graphic-step end-x]
-  (map (integration-operator func integration-step) (range 0 (+ end-x graphic-step) graphic-step)))
-
-(defn calc-integral-graphic-lazy-slow [func integration-step graphic-step end-x]
-  (map (integration-operator-slow func integration-step) (range 0 (+ end-x graphic-step) graphic-step)))
-
-
+  (map (integration-operator func integration-step) (range 0 end-x graphic-step)))
 
 
 
@@ -85,14 +61,14 @@
   ([func] (integration-operator-no-opti func fixed_h)))
 
 (defn calc-integral-graphic-no-opti [func integration-step graphic-step end-x]
-  (map (integration-operator-no-opti func integration-step) (range 0 (+ end-x graphic-step) graphic-step)))
+  (map (integration-operator-no-opti func integration-step) (range 0 end-x graphic-step)))
 
 
 (defn -main [& args]
   (println "Time testing:")
   (let [graphic-step 0.1
         integration-step 0.1
-        border 100]
+        border 200]
     (println (str "Graphic of cos from 0 to " border " with step " graphic-step "; integration step " integration-step))
     (doall (calc-integral-graphic-no-opti cos integration-step graphic-step 0.2))
     (doall (calc-integral-graphic-lazy cos integration-step graphic-step 0.2))
@@ -101,5 +77,3 @@
     (time (doall (calc-integral-graphic-no-opti cos integration-step graphic-step border)))
     (println "LAZY SEQ - ITERATE:")
     (time (doall (calc-integral-graphic-lazy cos integration-step graphic-step border)))))
-
-
