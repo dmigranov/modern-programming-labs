90904d151f788db97e9e8a73c5f30c09ad3bb58f
dmigranov <d.migranov@g.nsu.ru>
2020-10-10 19:00:01 +0700

    дележка на число тредов

--- lab3/lab3_1/src/lab3_1/core.clj
+++ lab3/lab3_1/src/lab3_1/core.clj
@@ -1,6 +1,8 @@
 (ns lab3-1.core
   (:gen-class))
 
+(def thread-count 4)
+
 (defn my-filter [pred coll]
   (reduce
    (fn [acc elem]
@@ -9,17 +11,19 @@
        acc))
    (list) coll))
 
-(defn my-filter-future [pred coll]
-  )
-
 (defn divide-collection [n acc rest]
   (if (> (count rest) 0)
     (recur n (concat acc (list (take n rest))) (drop n rest))
     acc))
 
-(defn my-partition [n coll] ;каждый кусок из n элементов
-  (divide-collection n (list) coll)
+(defn my-partition [n coll] ;каждый кусок из n элементов NOT LAZY
+  (divide-collection n (list) coll))
+
+
+(defn my-filter-future [pred coll]
+  (my-partition (Math/ceil (/ (count coll) thread-count)) coll)
   )
+
 
 ;deref = @
 ;When applied to a future, will block if computation not complete. then return value
