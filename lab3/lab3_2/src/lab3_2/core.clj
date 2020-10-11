00e51ebc779288f9f9f092c41c71100e6c0b0463
dmigranov <d.migranov@g.nsu.ru>
2020-10-11 18:58:42 +0700

    ладно потом

--- lab3/lab3_2/src/lab3_2/core.clj
+++ lab3/lab3_2/src/lab3_2/core.clj
@@ -2,12 +2,18 @@
   (:gen-class))
 
 ;lazy: iterate [текущая-часть, хвост]
+;(defn my-partition [n coll]
+;  (->>
+;   (iterate (fn [[current-part tail]] [(take n tail), (drop n tail)]) [(take n coll) (drop n coll)])
+;   (map first)
+;   (map (fn [elem] (if (= elem (list)) nil elem)))
+;   ) ;если конечная - то take k - количество списков
+;  )
+;превращает конечную в бесконечную...
+
 (defn my-partition [n coll]
-  (->>
-   (iterate (fn [[current-part tail]] [(take n tail), (drop n tail)]) [(take n coll) (drop n coll)])
-   (map first)
-   (take k)) ;k - количество списков
-  )
+(lazy-seq ))
+;todo
 
 
 (defn -main
