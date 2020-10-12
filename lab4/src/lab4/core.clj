707e0142640cb007dec03312d202cbd60ad0ca8d
dmigranov <d.migranov@g.nsu.ru>
2020-10-12 14:19:19 +0700

    начаты работы по лабе 4

--- lab4/src/lab4/core.clj
+++ lab4/src/lab4/core.clj
@@ -1,5 +1,34 @@
 (ns lab4.core
   (:gen-class))
+; :  - keyword
+; :: - keyword в пользовательском неймспейсу
+
+(defn variable [name]
+  {:pre [(keyword? name)]}
+  (list ::var name))
+
+(defn variable? [expr]
+  (= (first expr) ::var))
+
+(defn variable-name [var]
+  (second name))
+
+(defn same-variables? [v1 v2]
+  (and 
+   (variable? v1)
+   (variable? v2)
+   (= (variable-name v1) (variable-name v2))))
+
+;constant logical values (false and true) as operators of arity zero...
+
+(defn log-or [expr1 expr2]
+  (list ::or expr1 expr2)
+  )
+
+(defn log-or? [expr] 
+  (= ::sum (first expr)))
+
+(defn args [expr] (rest expr))
 
 (defn -main
   "I don't do a whole lot ... yet."
