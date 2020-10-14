f2f0e3d87c7bbcc0902f0e4d5b632442e76cf77e
dmigranov <d.migranov@g.nsu.ru>
2020-10-14 21:45:37 +0700

    старая функция partition закоменчена

--- lab3/lab3_2/src/lab3_2/core.clj
+++ lab3/lab3_2/src/lab3_2/core.clj
@@ -8,7 +8,6 @@
   (lazy-seq
    (cons 1 (map inc naturals))))
 
-
 ;из 3.1: не lazy, так как reduce и он будет пытаться всё размотать - StackOverflow
 (defn my-filter [pred coll] (reduce
    (fn [acc elem] (if (pred elem) (concat acc (list elem)) acc))
@@ -22,16 +21,15 @@
                 (cons (first s) (my-filter-lazy pred (rest s)))
                 (my-filter-lazy pred (rest s))))))
 
-
-;плохо: превращает конечную в бесконечную
-;(defn my-partition [n coll]
-;  (->>
-;   (iterate (fn [[current-part tail]] [(take n tail), (drop n tail)]) [(take n coll) (drop n coll)])
-;   (map first)
-;   (map (fn [elem] (if (= elem (list)) nil elem)))
-;   ) ;если конечная - то take k - количество списков
-;  )
-
+(comment ;плохо: превращает конечную в бесконечную
+(defn my-partition-old [n coll]
+  (->>
+   (iterate (fn [[current-part tail]] [(take n tail), (drop n tail)]) [(take n coll) (drop n coll)])
+   (map first)
+   (map (fn [elem] (if (= elem (list)) nil elem)))
+   ) ;если конечная - то take k - количество списков
+  )
+ )
 
 (defn my-partition [n coll]
   (lazy-seq (when-let [s (seq coll)]
