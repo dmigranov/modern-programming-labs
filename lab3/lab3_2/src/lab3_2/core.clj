66d71601ad4bf23268d4e1861613dfa8815fbe2f
dmigranov <d.migranov@g.nsu.ru>
2020-10-14 21:22:18 +0700

    my-filter-lazy done

--- lab3/lab3_2/src/lab3_2/core.clj
+++ lab3/lab3_2/src/lab3_2/core.clj
@@ -3,6 +3,10 @@
 
 (def base-thread-number 4)
 
+;for testing
+(def naturals
+  (lazy-seq
+   (cons 1 (map inc naturals))))
 
 (defn my-filter [pred coll] 
   (reduce
@@ -11,13 +15,18 @@
        (concat acc (list elem))
        acc))
    (list) coll))
-
+;(def filt-nat (my-filter even? naturals))
+;(take 10 filt-nat)
+;Error printing return value (StackOverflowError) (он пытается реализовать всю коллекцию в reduce)
 ;todo: make lazy
+
 (defn my-filter-lazy [pred coll]
   (lazy-seq (when-let [s (seq coll)]
-              (if (pred (first s)))
-              )
-  )
+              (if (pred (first s))
+                (cons (first s) (my-filter-lazy pred (rest s)))
+                (my-filter-lazy pred (rest s))))))
+;а тут все работает
+
 
 ;lazy: iterate [текущая-часть, хвост]
 ;(defn my-partition [n coll]
@@ -35,10 +44,10 @@
   (lazy-seq (when-let [s (seq coll)]
               (cons (take n s) (my-partition n (drop n s))))))
 
-(defn my-filter-future-lazy
-  ([pred coll thread-number] (->>
-                              ))
-  ([pred coll] (my-filter-future-lazy pred coll base-thread-number)))
+;(defn my-filter-future-lazy
+;  ([pred coll thread-number] (->>
+;                              ))
+;  ([pred coll] (my-filter-future-lazy pred coll base-thread-number)))
 
 
 (defn -main
